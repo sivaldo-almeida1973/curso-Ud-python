cf7d3c6b65561b0f96bdc8d112c644271661dcf1
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-08 09:10:37 -0300

    Create conversao.py

---
+++ conversao.py
@@ -0,0 +1,22 @@
+"""
+conversão de tipos , coerção
+type convertion, typecastyng, coercion
+é o ato de converter um tipo em outro
+
+Tipos imutaveis e primitivos:
+str , int, float, bool
+"""
+#gera um erro
+#print('1'+1)
+print('1', type('1'))
+
+
+
+#concatenação
+print('1'+'1')
+print('a'+'b')
+
+#Conversão
+print(int('1'), type(int('1')))
+print(float('1'), type(float('1')))
+
