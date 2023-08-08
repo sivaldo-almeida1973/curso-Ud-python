cffa6e525485cae021d4ff4bb39f8055d9da15a9
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-07 21:58:51 -0300

    Create funcaoPrint.py

---
+++ funcaoPrint.py
@@ -0,0 +1,14 @@
+'''
+sep -> separador
+
+CRLF - \r\n
+
+'''
+
+print(12,34,1011,  sep='-', end='##\n')
+print(12,34, sep='=')
+print(12,34, sep='/')
+print(12,34)
+print(12,34)
+print(12,34)
+print(12,34)
