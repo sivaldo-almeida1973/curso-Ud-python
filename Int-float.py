38869aba8454d3b18b3d8a9e67c9b52113a19e0e
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-07 22:10:57 -0300

    Create Int-float.py

---
+++ Int-float.py
@@ -0,0 +1,31 @@
+#tipos int e float
+#int - numero inteiro
+'''
+qualquer numero positivo ou negativo
+
+tipos primitivos
+'''
+print(11)
+print(-11)
+print(0)
+
+
+#float - numero com ponto flutuante
+"""
+o tipo float representa qualquer numero
+positivo ou negativo com ponto flutuante
+
+"""
+
+print(1.1, 10.11)
+print(0.0)
+print(-1.5)
+
+#a classe type mostra o tipo que o python inferiu
+
+print( type('sivaldo'))
+print(  type(123)   )
+print(  type(-1)   )
+print(     )
+print(     )
+print(     )
