2b444da25e48ea6f8066e4370ff4a1a90ccad3ef
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-07 21:58:53 -0300

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,23 @@
+'''
+python = lingagem de programação
+tipo de tipagem = dinamica / forte
+str - strings - texto
+strings são textos dentro de aspas
+'''
+#aspas simples
+print('123')
+print('sivaldo')
+
+#aspas duplas
+print("1234")
+print('sivaldo')
+
+# aspas dentro de aspas faz mostrar 
+print('sivaldo \"vieira" ')
+
+#o (r) faz mostrar os caracteres
+print(r'sivaldo \"vieira\" ')
+
+
+
+print(2,"sivaldo 'vieira'")
