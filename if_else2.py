d712303ef69cc55bcf90c2ae67880568701bcbc1
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-31 18:48:27 -0300

    Create if_else2.py

---
+++ if_else2.py
@@ -0,0 +1,22 @@
+# if , / elif       ,   /else
+# se , / se não se  ,  /se nao
+
+condicao1 = False
+condicao2 = True
+condicao3 = False
+condicao4 = False
+
+
+if condicao1:
+  print('este é o codigo do if 1!')
+  print('este é o codigo do if 1!')
+elif condicao2:
+  print('A condicao 2 é  verdadeira!')
+  print('A condicao 2 é  verdadeira!')
+elif condicao3:
+  print('A condicao 3 é  falso!')
+elif condicao4:
+  print('A condicao 4 é  falso!')
+else:
+  print('tudo é falso!')
+
