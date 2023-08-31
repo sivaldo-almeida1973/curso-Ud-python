1522d2e0a5c40e702bc5b22d87ae50be1555868d
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-31 18:37:09 -0300

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,14 @@
+# if , / elif       ,   /else
+# se , / se não se  ,  /se nao
+
+entrada = input('Voce quer "entrar" ou "sair" ?')
+
+if entrada == 'entrar':
+  print('Voce entrou')
+  print(11111111111111111)
+elif entrada == 'sair':
+  print('Voce saiu!')
+else :
+  print('Opção invalida')
+
+
