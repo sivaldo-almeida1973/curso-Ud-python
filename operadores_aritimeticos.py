96dba2ec0ca2dbe2ebd385f713fe1a4fcbe79373
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-31 13:11:15 -0300

    Create operadores_aritimeticos.py

---
+++ operadores_aritimeticos.py
@@ -0,0 +1,23 @@
+adicao = 10 + 10
+print('Adição:', adicao)
+
+
+subtracao = 10 - 5
+print('Subtração:', subtracao)
+
+
+multiplicação = 10 * 3
+print('Multiplicação:', multiplicação)
+
+
+
+divisao = 10 / 3
+print('Divisão:', divisao)
+
+
+divisao_inteira = 10 // 3
+print('Divisão inteira:', divisao_inteira)
+
+
+Equação = 10 ** 2
+print('Equação:', Equação)
