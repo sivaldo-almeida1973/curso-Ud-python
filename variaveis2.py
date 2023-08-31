095885e31a775823e75c45cd9b09f3716dfbeb10
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-31 13:11:08 -0300

    Update variaveis2.py

--- variaveis2.py
+++ variaveis2.py
@@ -1,9 +1,9 @@
 nome = 'sivaldo'
 sobrenome = 'vieira de almeida'
 idade = 30
-anoNascimento = 1990
+anoNascimento = 1973
 idadeCerta = 2023 - anoNascimento
-altura = 1.70
+altura_metros = 1.70
 idadePassada = idadeCerta == idade
 
 print('nome:', nome)
@@ -11,5 +11,5 @@ print('sobrenome:', sobrenome)
 print('idade:', idade)
 print('ano nascimento:', anoNascimento)
 print('maior idade:', idadeCerta)
-print('altura:', altura)
+print('altura:', altura_metros)
 print('idade passada', idadePassada)
