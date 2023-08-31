2c20ff0ce1211b7664109e843ea5d9997d827a81
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-31 16:48:13 -0300

    Create funcao_input.py

---
+++ funcao_input.py
@@ -0,0 +1,21 @@
+# nome = input('qual o seu nome:')
+#print(f'O seu é: {nome =}')
+
+num1 = input('digite um numero:')
+num2 = input('digite outro numero:')
+
+int_nummero_1 = int(num1)
+int_nummero_2 = int(num2)
+
+soma = int_nummero_1 + int_nummero_2
+
+
+
+
+
+#num1 = input('digite um numero:')
+#num2 = input('digite outro numero:')
+
+
+print(f'A soma dos numeros {int_nummero_1} e {int_nummero_2} é {soma}')
+
