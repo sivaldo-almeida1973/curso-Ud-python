78ee0470b6d0ecd0cc1368dbc3298bae9905073c
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-31 15:35:43 -0300

    Create precedencia.py

---
+++ precedencia.py
@@ -0,0 +1,11 @@
+# 1º.     (n+n)     parenteses
+# 2º.      **       potencia
+# 3º.     * / // %
+# 4º.     +, -
+
+conta_1 = 1 + 1 ** 5 + 5
+
+conta_2 =( 1 + 1) **( 5 + 5)
+
+print(conta_1)
+print(conta_2)
