843d7d5ccace2fb2a0a191e435eb380e7ef0ceac
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-31 16:09:29 -0300

    Create exercicio_imc.py

---
+++ exercicio_imc.py
@@ -0,0 +1,8 @@
+nome = 'sivaldo vieira'
+altura = float(1.700)
+peso = 86
+imc = peso/(altura * altura)
+
+
+print(f'Seu nome é: {nome}')
+print(f'O seu peso é {peso}KG e sua altura é {altura}M por isso seu imc é: {imc}')
