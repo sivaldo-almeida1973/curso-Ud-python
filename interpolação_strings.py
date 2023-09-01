ea0deb7baeb20f7e676b063d79f7b97800360c41
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-09-01 10:55:44 -0300

    Create interpolação_strings.py

---
+++ interpolação_strings.py
@@ -0,0 +1,30 @@
+"""
+Interpolação basica de strings
+s - string
+
+d e i - int
+f - float
+x e X - hexadecimal (abcdef0123456789)
+
+
+"""
+
+
+
+nome = 'sivaldo'
+preco = 1000.9776543
+
+variavel = '%s, o preco total foi R$ %.2f ' % (nome, preco)
+
+print(variavel)
+
+print('O hexadecimal de %d é %04X' % (15, 15))
+
+
+
+
+
+
+
+
+
