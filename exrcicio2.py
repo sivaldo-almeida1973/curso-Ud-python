d16188005b01bac1bb486b0b9366d5fc06d14ef4
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-09-01 10:10:15 -0300

    Update exrcicio2.py

--- exrcicio2.py
+++ exrcicio2.py
@@ -7,7 +7,7 @@ int_segundo  = int(segundo_valor)
 if int_primeiro > int_segundo:
   print(f'Primeiro valor {int_primeiro} é maior que segundo valor {int_segundo}')
 elif int_segundo > int_primeiro:
-  print(f'Segundo valor {int_segundo} é maior primeiro valor {int_primeiro}!')
+  print(f'Segundo valor {int_segundo} é maior que primeiro valor {int_primeiro}!')
 else:
   print(f'Valores digitados {int_primeiro} e {int_segundo} São iguais!')
 
