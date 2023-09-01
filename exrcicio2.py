77cc7cc2e17618a9f7b073e23ddcafc77c0d8f4f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-31 22:39:04 -0300

    Create exrcicio2.py

---
+++ exrcicio2.py
@@ -0,0 +1,13 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+int_primeiro = int(primeiro_valor)
+int_segundo  = int(segundo_valor)
+
+if int_primeiro > int_segundo:
+  print(f'Primeiro valor {int_primeiro} é maior que segundo valor {int_segundo}')
+elif int_segundo > int_primeiro:
+  print(f'Segundo valor {int_segundo} é maior primeiro valor {int_primeiro}!')
+else:
+  print(f'Valores digitados {int_primeiro} e {int_segundo} São iguais!')
+
