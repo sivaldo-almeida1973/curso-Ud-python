d8b12b79c20aecd3481824170393010264b22c13
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-09-04 12:42:28 -0300

    Create fatiamento_strings.py

---
+++ fatiamento_strings.py
@@ -0,0 +1,26 @@
+"""
+Fatiamento de strings
+
+[i:f:p] [::]
+
+len -> retorna qtde de caractere
+"""
+
+variavel = 'Olá, mundo!'
+print(variavel[-4]) #imprimir o indice -4
+print(variavel[4]) #imprimir o indice 4
+print(variavel[4:]) # do 4 ate o final
+print(variavel[:4]) # do inicio ate o 3
+print(variavel[4:8])
+print(variavel[0:11:1]) #imprimir o indice -4
+print(variavel[0:11:2]) #imprimir do 0 ao 11 (de 2 em 2)
+print(variavel[::-1]) #imprimir ao contrario
+
+
+print(len(variavel)) # quantas caracteres tem na strings
+
+
+
+
+
+
