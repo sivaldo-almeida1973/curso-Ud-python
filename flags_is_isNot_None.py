8205018f6f8c5c7bacf1a3e126eb82f28f1b78b2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-09-05 17:20:52 -0300

    python

---
+++ flags_is_isNot_None.py
@@ -0,0 +1,16 @@
+"""
+flag (bandeira - marcar um local)
+
+none = não valor
+is  is not = é ou não é(tipo ,valor ,identidade)
+
+id -= identidade
+
+"""
+
+condicao = False
+
+if condicao:
+  print('Faça algo!')
+else:
+  print('não faça algo!')
---
+++ identidade_valor_memoria.py
@@ -0,0 +1,17 @@
+"""
+flag (bandeira - marcar um local)
+
+none = não valor
+is  is not = é ou não é(tipo ,valor ,identidade)
+
+id -= identidade
+
+"""
+
+v1 = 'b'
+v2 = 'c'
+v3 = 'd'
+
+print(f'O id de v1 é:',id(v1))
+print(f'O id de v1 é:',id(v2))
+print(f'O id de v1 é:',id(v3))
