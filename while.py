2bcea19b6b663542a32fcf23980a4c393582275b
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-09-08 22:17:29 -0300

    Create while.py

---
+++ while.py
@@ -0,0 +1,22 @@
+"""
+While (enquanto)
+executa enquanto for verdade
+"""
+
+
+condicao = True
+
+while condicao:
+  nome = input('qual seu nome? ')
+  print('seu nome é {nome}')
+
+
+  if nome == 'sair':
+   break
+
+print('acabou!')
+
+
+
+
+  
