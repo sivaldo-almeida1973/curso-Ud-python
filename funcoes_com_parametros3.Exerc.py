996a368b0c43af2da8670eaba16576a2d3df3315
sivaldo vieira de almeida <vieiralmeida.com@hotmail.com>
2023-10-14 21:33:31 -0300

    Create funcoes_com_parametros3.Exerc.py

---
+++ funcoes_com_parametros3.Exerc.py
@@ -0,0 +1,14 @@
+"""
+
+
+"""
+
+#Exemplo 3
+
+def separar(lista):
+  for num in lista:
+    if num > 10:
+      print(num, end=' ')
+
+listaCriada = [21,3,213,543, 54,65,12 ,0,23 ,432]
+separar(listaCriada)
