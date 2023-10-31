3c4406efdad82088f201f8023da3bfd783491fa9
sivaldo vieira <vieiralmeida.com@hotmail.com>
2023-10-31 20:49:37 -0300

    Update funcoes_com_parametros4.Exerc.py

--- funcoes_com_parametros4.Exerc.py
+++ funcoes_com_parametros4.Exerc.py
@@ -26,10 +26,7 @@ def soma(num1, num2):
 
 soma(10,18)
 
-
-
-"""
-
+===================================
 
 
 #função com parametro padrão (default)
@@ -42,6 +39,57 @@ def medida(numero, referencia= 60):
 
 
 medida(70)
+medida(50)
+medida(100)
+medida(30)
+medida(200)
+medida(0)
+
+
+======================================
+
+def medida(numero=76, referencia= 60):
+  if numero > referencia:
+    print(f'{numero} é maior que {referencia}')
+  else:
+    print(f"{numero} é menor que {referencia}")
+
+medida()
+medida(40,50)      #se nao passar parametros ,vai valer isso
+medida(70)         #se nao passar parametros ,vai valer isso
+
+=================================
+# Comando global
+nome = 'arroz'  #variavel global----------
+
+def comida():
+  global nome  #Comando global converte em varivel local
+  nome = nome + " e miojo"
+  print(nome)
+
+comida()
+
+
+
+"""
+
+
+
+
+# Comando local(funcao dentro de funcao)================
+
+def funFora(): #criar funcao
+  total = 0 #criar variavel 
+  def funDentro(): #criar funcao dentro
+    nonlocal total #faz variavel da funcao cima valer aqui
+    total = total + 1
+    print(total)
+  return funDentro()  #pertence a funFora, retorna e fecha
+
+funFora()
+
+
+
 
 
 
