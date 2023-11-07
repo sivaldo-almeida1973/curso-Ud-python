b3e5ffad2b3fc337046950628d59271626414ba9
sivaldo vieira <vieiralmeida.com@hotmail.com>
2023-11-06 21:28:57 -0300

    Create Mod_teste.py

---
+++ Mod_teste.py
@@ -0,0 +1,15 @@
+"""
+Modulos customizados externos
+
+
+
+"""
+
+def imparPar(numero):
+  if numero % 2 == 0:
+    print(f'{numero} é par')
+  else:
+    print(f'{numero} é imapr')
+
+
+lista = [1,2,3,4,5,6,7]
