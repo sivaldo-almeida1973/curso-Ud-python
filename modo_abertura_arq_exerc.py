74846f3117e25e1de15d68be15931d390be241c8
sivaldo vieira <vieiralmeida.com@hotmail.com>
2023-11-10 13:05:55 -0300

    Create modo_abertura_arq_exerc.py

---
+++ modo_abertura_arq_exerc.py
@@ -0,0 +1,4 @@
+"""
+Teste se um arquivo chamado livros.txt não exista pela abertura x, caso o arquivo ja exista abra com w+, utilize try/except nessa manipulação.Imprima na tela qual foi o modo de abertura,escreve no bloco o nome dos tres melhores livros que voce ja leu (um em cada linha) adicionando o arquivo, feche-o .Abra-o novamente e adicione mais um livro ao final do arquivo sem apaga-lo, por fim , leia a versão final.
+
+"""
