75db937e348b72be1fcccb930859edf69d0017ca
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-14 20:39:30 -0300

    Update Atributos.py

--- POO/Atributos.py
+++ POO/Atributos.py
@@ -115,12 +115,7 @@ posche = Carro(300,350,5)
 posche.cria_atributo()
 print(posche.pr)
 
-
-
-
-"""
-
-
+===================como deletar atributos===============
 
 class Carro:
 
@@ -139,8 +134,91 @@ class Carro:
 
 corolla = Carro(250, 200, 2)
 posche = Carro(350, 300, 2)
+posche.preco = 300000#variavel dinamica
+print('Antes do del==================================:')
+print(f'Corolla:',corolla.__dict__)
+print(f'Posche: ',posche.__dict__)
 
-print(corolla.__dict__)
-print(posche.__dict__)
+del corolla.estado
+del posche.estado
+del posche.preco
+
+print('depois do del:==================================:')
+print(f'Corolla:',corolla.__dict__)
+print(f'Posche: ',posche.__dict__)
+
+
+=====================Subdivisão: atributos Publicos x Privados=============
+
+-Na teoria ,atributos publicos podem ser acessados por todos, enquanto o privado não.
+
+-Como Declarar?
+
+self.potencia = potencia #Declarei um atributo publico
+
+self.__potencia = potencia #declarei um atributo privado (Dois anderline)
+
+
+class Carro:
+
+  #atributo de classe--------------------nitro
+  nitro = 1.1 #aumentar velocidade de cada obj em 10%
+
+  #metodo construtor -recebe com atributos de instancia
+  def __init__(self, vel_maxima, potencia, num_bancos):
+    self.__vel_maxima = vel_maxima * Carro.nitro# atributo privado de instanc
+    self.potencia = potencia  # atributo publico de instanc
+    self.num_bancos = num_bancos
+    self.estado = False #variavel de estado se esta ligado ou não
+  
+ 
+
+
+corolla = Carro(250, 200, 2)
+posche = Carro(350, 300, 2)
+print(posche.potencia)      #publico aceita acesso
+#print(posche.__vel_maxima)  #atributo privado não pode ser acessado assim
+print(posche._Carro__vel_maxima)  #essa é a forma de acessar atrib privado
+print(corolla._Carro__vel_maxima)  #essa é a forma de acessar atrib privado
+#name Mangling: obj._classe__atributo
+
+
+
+
+
+"""
+
+
+class Carro:
+
+  #atributo de classe--------------------nitro
+  nitro = 1.1 #aumentar velocidade de cada obj em 10%
+
+  #metodo construtor -recebe com atributos de instancia
+  def __init__(self, vel_maxima, potencia, num_bancos):
+    self.__vel_maxima = vel_maxima * Carro.nitro# atributo privado de instanc
+    self.potencia = potencia  # atributo publico de instanc
+    self.num_bancos = num_bancos
+    self.estado = False #variavel de estado se esta ligado ou não
+  
+  #funcao liagar e desligar o carro
+  def liga_desliga(self):
+    if self.estado == False:
+      self.estado = True
+    else:
+      self.estado = False
+ 
+
+
+corolla = Carro(250, 200, 2)
+print(corolla.estado)
+corolla.liga_desliga() #ligar o carro
+print(corolla.estado)
+
+corolla.liga_desliga() #desliga o carro
+print(corolla.estado)
+
+
+
 
 
