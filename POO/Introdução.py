840875d720732c9b040f525ce49b9d330dde556f
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-13 21:29:09 -0300

    Create Introdução.py

---
+++ POO/Introdução.py
@@ -0,0 +1,52 @@
+"""
+PARADIGMAS======================
+
+Programação Orientada a Objetos:
+
+
+
+#Ate agora vimos programação estrutural,que consiste em uma sequencia de comandos que são interpretados linearmente.
+
+-Programação funcional:
+
+=Funcoes Matematicas(imutaveis)utilizadas por bancos.
+
+-Programação Orientada a objetos.============================
+
+Vamos estudar agora....
+
+a)classe: conjunto de métodos e atributos que constroem o objeto.
+
+b)Metodo: São como funções, porem estão dentro da classe.
+-Metodos desempenham o papel de controlar/interagir com o Objeto.
+
+C)Atributo: Caracteristicas do objeto que a classe irá controlar.
+
+D)Objeto: Pessoa/Animal/Objeto da vida real que será controlado.
+
+
+
+1)Encapsulamento:
+-Promove maior segurança do programa, o desenvolvimento do código em POO é delimitado por classe,  limitando o alcance de determinados atributos pelo obj.
+
+EX:segurança da conta bancária, não deixando qualquer usuário alterar o valor do seu saldo na conta.
+
+
+2) Abstração:
+-Ocultação de códigos que não são necessários para o usuário visualizar.
+
+Ex: O usuário não vê toda lógica do programa, apenas a execução dos mecanismos que deseja.
+
+3) Herança: é possível criar sub-classes podendo manter um controle de classses mais importantes controlando classes menores.
+
+Ex: filho recebe algumas características da classe Pai (cor do cabelo, por exemplo).
+
+4)Polimorfismo: como o proprio nome sugere, os obj podem asumir mais de uma forma, permitindo usar um mesmo código para vários tipos de dados.
+
+EX: Se tem uma classe que calcula a área de uma forma geométrica por diferentes métodos, essa classe pode calcular para diferentes figuras geométricas(Triangulo, Trapezio,Circulo).
+
+
+
+
+
+"""
