13c29597f56c28ed3a890d39b75ad5d3b2221185
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-16 21:32:41 -0300

    Create Heranca_exercicio.py

---
+++ POO2/Heranca_exercicio.py
@@ -0,0 +1,10 @@
+
+"""
+1 - Desenvolva o sistema de um controle universal para uma casa. O controle deve ser a Classe-Mãe, que 
+contém o método liga/desliga e é herdada por outras três classes (equipamentos controlados): 
+ar-condicionado, micro-ondas e televisão, que controlam, respectivamente, temperatura, tempo e volume em 
+métodos dentro das classes. Além disso, os métodos construtores das Classes Filhas recebem a variável 
+controlada em seu valor atual, por exemplo 'temperaturaAtual'. 
+Obs.: Utilize também propriedades para realizar o acesso aos atributos.
+
+"""
