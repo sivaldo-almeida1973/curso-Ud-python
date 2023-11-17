eaa6797ceadebfde5568e798b36e86114e53ab23
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-17 12:06:05 -0300

    Update Heranca_exercicio.py

--- POO2/Heranca_exercicio.py
+++ POO2/Heranca_exercicio.py
@@ -8,3 +8,100 @@ controlada em seu valor atual, por exemplo 'temperaturaAtual'.
 Obs.: Utilize também propriedades para realizar o acesso aos atributos.
 
 """
+class Controle:
+  
+    
+    ligado = False
+
+    #metodo -----
+    def liga_desliga(self):
+      self.ligado = not self.ligado
+
+#inicio da classe arcondiconado   ------------------------  
+class Arcondicionado(Controle):
+   #metodo construtor
+   def __init__(self, temperaturaAtual) -> None:
+      super().__init__()
+      self.__temperaturaAtual = temperaturaAtual
+
+   #metodo controle da temperatura
+   def controleTemperatura(self, temperatura):
+      self.__temperaturaAtual = temperatura
+
+  #propriedade para acessar o atributo privado
+   @property
+   def temperaturaATual(self):
+      return f'Temperatura atual:  {self.__temperaturaAtual}'
+
+#final da classe arcondiconado   ------------------------  
+
+
+class Microondas(Controle):
+   
+   def __init__(self, tempoAtual):
+      super().__init__()
+      self.__tempoAtual = tempoAtual
+
+   def controleTempo(self, tempo):
+      self.__tempoAtual = tempo
+
+   @property
+   def tempoAtual(self):
+      return f'Tempo atual: {self.__tempoAtual}'
+    
+
+#final da classe micro_ondas   ------------------------  
+
+
+class Televisao(Controle):
+   
+   def __init__(self,volumeAtual):
+      super().__init__()
+      self.__volumeAtual = volumeAtual
+
+   def controleVolume(self, volume):
+      self.__volumeAtual = volume
+
+   @property
+   def volumeAtual(self):
+      return f'Volume arual: {self.__volumeAtual}'
+   
+
+
+#criar obj
+
+arc = Arcondicionado(45)
+mic = Microondas(60)
+tv = Televisao(86)
+
+print(arc.ligado)
+arc.liga_desliga()
+print(arc.ligado)
+print(arc.temperaturaATual)
+arc.controleTemperatura(35)
+print(arc.temperaturaATual)
+
+
+print('\n')
+print(mic.ligado)
+mic.liga_desliga()
+print(mic.ligado)
+print(mic.tempoAtual)
+mic.controleTempo(50)
+print(mic.tempoAtual)
+
+
+print('\n')
+print(tv.ligado)
+tv.liga_desliga()
+print(tv.ligado)
+print(tv.volumeAtual)
+tv.controleVolume(100)
+print(tv.volumeAtual)
+
+
+
+
+
+
+      
