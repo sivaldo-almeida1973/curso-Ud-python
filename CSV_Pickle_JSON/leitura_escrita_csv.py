721896e635300b71ade66b873619e307a8266f99
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-19 19:31:27 -0300

    lendo arquivos

--- CSV_Pickle_JSON/leitura_escrita_csv.py
+++ CSV_Pickle_JSON/leitura_escrita_csv.py
@@ -13,13 +13,27 @@ http://dados.gov.br/dataset
 
 """
 
-#Abrindo arquivo csv para leitura não pythonico
+#Abrindo arquivo csv para leitura (Modo não-pythonico)
 
 with open('pessoas_famosas.csv') as arq:
-  informacoes = arq.readline() #ler linha por linha
-  print(type(informacoes))
-  print(informacoes)
+  informacoes = arq.readlines() #ler linha por linha
+  print(type(informacoes))#lista
+  print(informacoes) #cada indice representa uma linha
+  print('\n')
+
+  print('trabalhar dados especificos!!\n')
   for elemento in informacoes:
     print(elemento, end="")
   del informacoes[0]
   print(informacoes)
+#passar um filtro
+  del informacoes[0] #deletar cabeçalho
+  print('\n')  #sem o cabeçalho
+  print(informacoes)
+  print('\n====================')
+  #separar elementos
+  for indice, conteudo in enumerate(informacoes):
+    print(indice)
+    print(conteudo)
+
+
