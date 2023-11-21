3f712cd4b09b3f4847f3e5fe2afec2fd9d7847f0
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-21 10:52:28 -0300

    doctests

--- testes/doctests.py
+++ testes/doctests.py
@@ -9,20 +9,16 @@ Doctests:
 
 Ex1:
 
-
-
-
-"""
 from math import pi
 
 
 def circulo(raio):
-  """
+  
   Calcula a area em cm² de um circulo
   raio: Raio em cm² do circulo
-  >>> circulo(2)
+  #>>> circulo(2)
   12.57
-  """
+  
 
   area = round(pi*(raio ** 2), 2)
   return area
@@ -30,14 +26,59 @@ def circulo(raio):
 
 
 def retangulo(base, altura):
-  """
+  
   Calcula a area em cm² de um retangulo
-  >>> retangulo(2,2)
+ # >>> retangulo(2,2)
   4
   
-  """
+  
   area = base*altura
   return area
 
 print(circulo(7))
 print(retangulo(3,3))
+
+=========================================================
+
+Ex2:
+class Operacoes:
+
+    def __init__(self, valor):
+      self.__valor = valor
+
+
+    def incrementa(self):
+      
+      Aumenta em 1 o valor do objeto
+
+     # >>> numero = Operacoes(2)
+     # >>> numero.incrementa()
+      
+      3
+      
+      self.__valor += 1
+      return self.__valor
+
+
+    def decrescimo(self):
+      
+      Diminui em 1 o valor do objeto
+
+     # >>> numero = Operacoes(2)
+     # >>> numero.decrescimo()
+
+      1
+      
+      self.__valor -= 1
+      return self.__valor
+
+
+"""
+
+def gritar(palavra):
+  """
+  Transforma a palavra em letras maiusculas
+  >>> gritar('gollllll')
+  '''GOLLLLLL'''
+  """
+  return f'''{palavra.upper()}'''
---
+++ testes/doctests_exerc.py
@@ -0,0 +1,7 @@
+def gritar(palavra):
+  """
+  Transforma a palavra em letras maiusculas
+  >>> gritar('gollllll')
+  '''GOLLLLLL'''
+  """
+  return f'''{palavra.upper()}'''
