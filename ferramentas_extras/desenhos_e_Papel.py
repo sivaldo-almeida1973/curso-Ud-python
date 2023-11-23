ea5083e5da746ef413e74296d914ecef5d9ef86f
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-23 20:23:31 -0300

    Update desenhos_e_Papel.py

---
+++ ferramentas_extras/desenhos_e_Papel.py
@@ -0,0 +1,4 @@
+import pywhatkit
+
+pywhatkit.text_to_handwriting('Nome: Judite\nDisciplina:Sociologia\nAtividade avaliativa 3',
+                              save_to=('exercicio.png'),rgb=(0, 0, 255))
