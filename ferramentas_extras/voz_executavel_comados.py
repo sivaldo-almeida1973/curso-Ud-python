862e8100f2bf7aa4670018edbebd3d5da00d3347
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2023-11-24 12:22:52 -0300

    Update voz_executavel_comados.py

--- ferramentas_extras/voz_executavel_comados.py
+++ ferramentas_extras/voz_executavel_comados.py
@@ -1,15 +1,15 @@
 ''''''
 '''______________________________Abrindo aplicativos usando a voz______________________________'''
 # # Abrindo aplicativos usando a voz
-#
+
 # # pip install pyttsx3, SpeechRecognition
 # # PyAudio -> pip install pipwin -> pipwin install pyaudio
 # from pyttsx3 import init
 # from speech_recognition import Recognizer, Microphone
 # from os import system
-#
+
 # class Comandos:
-#
+
 #     def assistente_ouvir(self):
 #         rec = Recognizer()
 #         with Microphone() as source:
@@ -24,12 +24,12 @@
 #                 print('_____Não estou ouvindo!')
 #                 return 'None'
 #         return palavras
-#
+
 #     def assistente_falar(self, falar):
 #         engine = init('sapi5')
 #         engine.say(falar)
 #         engine.runAndWait()
-#
+
 #     def assistente_acoes(self):
 #         while True:
 #             frase = self.assistente_ouvir()
@@ -38,23 +38,23 @@
 #                     print("_____Abrindo bloco de notas!")
 #                     self.assistente_falar("Abrindo bloco de notas!")
 #                     system("start notepad")
-#
+
 #                 elif "navegador" in frase:
 #                     print("_____Abrindo navegador!")
 #                     self.assistente_falar("Abrindo navegador!")
 #                     system("start firefox")
-#
+
 #                 elif "arquivos" in frase:
 #                     print("_____Abrindo arquivos!")
 #                     self.assistente_falar("Abrindo arquivos!")
 #                     system("start explorer")
-#
+
 #                 elif 'cmd' in frase:
 #                     print("_____Abrindo CMD!")
 #                     self.assistente_falar("Abrindo CMD!")
 #                     system("start cmd")
-#
-#
+
+
 # if __name__ == '__main__':
 #     assistente = Comandos()
 #     assistente.assistente_acoes()
@@ -63,10 +63,10 @@
 # Converter python em exe
 
 #pip install pyinstaller
-#Terminal: pyinstaller nomedoarquivo.py (diretório, Extras/Voz_executável_e_comandos_extras.py)
-#Terminal: pyinstaller --onefile nomedoarquivo.py
+# Terminal: pyinstaller nomedoarquivo.py (diretório, Extras/Voz_executável_e_comandos_extras.py)
+# Terminal: pyinstaller --onefile nomedoarquivo.py
 
-#Contendo janelas: Terminal: pyinstaller --onefile -w nomedoarquivo.py
+# Contendo janelas: Terminal: pyinstaller --onefile -w nomedoarquivo.py
 
 '''______________________________Identação múltipla______________________________'''
 # # Identação múltipla
